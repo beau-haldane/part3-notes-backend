4b3a13315245dc515b30eb31fffe839692e7cfcf
beau-haldane <beauhaldane@gmail.com>
2022-03-10 15:00:27 +1000

    adding repo to heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 let notes = [
   {
@@ -22,9 +23,21 @@ let notes = [
   }
 ]
 
-// test
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
 app.use(express.json())
+
+app.use(requestLogger)
+
+app.use(cors())
+
+app.use(express.static('build'))
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -80,7 +93,13 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
-const PORT = 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
