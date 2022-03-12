31e863c68ff672fd79a58aed0e95719a8291b281
beau-haldane <beauhaldane@gmail.com>
2022-03-12 11:07:15 +1000

    Trying to make Heroku serve index.html from build folder

--- index.js
+++ index.js
@@ -32,11 +32,8 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(express.json())
-
 app.use(requestLogger)
-
 app.use(cors())
-
 app.use(express.static('build'))
 
 app.get('/', (req, res) => {
