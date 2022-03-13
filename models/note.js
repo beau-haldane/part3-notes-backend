8e01d6c6a09204c6c796ed01f4e003e7f1d32a5b
beau-haldane <beauhaldane@gmail.com>
2022-03-13 15:51:30 +1000

    uibuild

---
+++ models/note.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+// Connect to MongoDB
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+// Set schema for notes
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+// Format object returned by MongoDB
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+// Export module and return Mongo model
+module.exports = mongoose.model('Note', noteSchema)
