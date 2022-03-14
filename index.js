5989622ec2096731034a83398696c55e7d73ce16
beau-haldane <beauhaldane@gmail.com>
2022-03-14 16:24:48 +1000

    uibuild

--- index.js
+++ index.js
@@ -3,105 +3,106 @@ const express   = require('express')
 const app       = express()
 const Note      = require('./models/note')
 const cors      = require('cors')
-const mongoose  = require('mongoose')
+require('mongoose')
 
 // --- MIDDLEWARE --- //
-  app.use(express.static('build'))
-  app.use(express.json())
-  app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
 
 
 // --- MONGODB --- //
 
-  // Set Mongo database as response for /api/notes
-  app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-      response.json(notes)
-    })
+// Set Mongo database as response for /api/notes
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
   })
+})
 
 // --- REQUEST LOGGER --- //
-  const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-  }
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
-  app.use(requestLogger)
+app.use(requestLogger)
 
 // --- HTTP REQUESTS --- //
 
-  // Get index 
-  app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-  })
+// Get index
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
 
-  // Create new note
-  app.post('/api/notes', (request, response, next) => {
-    const body = request.body
-    // Error if note is missing body content
-    if (body.content === undefined) {
-      return response.status(400).json({ error: 'content missing' })
-    }
-    // Create new note object
-    const note = new Note({
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-    })
-    // Add new note to notes array
-    note.save().then(savedNote => {
-      response.json(savedNote)
-    })
-    // Pass exceptions to error handler
+// Create new note
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+  // Error if note is missing body content
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+  // Create new note object
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+  // Add new note to notes array
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+  // Pass exceptions to error handler
     .catch(error => next(error))
-  })
+})
 
-  // Delete note
-  app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
+// Delete note
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+  // eslint-disable-next-line no-unused-vars
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  // Get note by id
-  app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-      .then(note => {
-        if (note) {
-          response.json(note)
-        } else {
-          response.status(404).end()
-        }
-      })
-      .catch(error => next(error))
-  })
+// Get note by id
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
 
-  // Update note
-  app.put('/api/notes/:id', (request, response, next) => {
-    const {content, important} = request.body
-  
-    Note.findByIdAndUpdate(
-      request.params.id, 
-      { content, important },
-      { new: true, runValidators: true, context: 'query' }
-    ) 
-      .then(updatedNote => {
-        response.json(updatedNote)
-      })
-      .catch(error => next(error))
-  })
+// Update note
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
 
 // --- UNKNOWN ENDPOINT HANDLING --- //
-  const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-  app.use(unknownEndpoint)
+app.use(unknownEndpoint)
 
 // --- ERROR HANDLING --- //
 const errorHandler = (error, request, response, next) => {
--- models/note.js
+++ models/note.js
@@ -6,13 +6,14 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
+// eslint-disable-next-line no-unused-vars
   .then(result => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
-  
+
 // Set schema for notes
 const noteSchema = new mongoose.Schema({
   content: {
@@ -20,7 +21,7 @@ const noteSchema = new mongoose.Schema({
     minLength: 5,
     required: true
   },
-  date: { 
+  date: {
     type: Date,
     required: true
   },
