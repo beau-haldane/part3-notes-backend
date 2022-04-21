89d81c0b356cbb3a95772dc3a8b8a0abbbb0186b
beau-haldane <beauhaldane@gmail.com>
2022-04-21 16:55:12 +1000

    completed section 4b

--- models/note.js
+++ models/note.js
@@ -1,19 +1,5 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-// Connect to MongoDB
-console.log('connecting to', url)
-
-mongoose.connect(url)
-// eslint-disable-next-line no-unused-vars
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 // Set schema for notes
 const noteSchema = new mongoose.Schema({
   content: {
